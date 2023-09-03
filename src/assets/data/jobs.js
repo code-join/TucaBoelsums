e982a5487b66a0e9b083ea176bea0a97dd855b63
jpcp0614 <jpcp0614@gmail.com>
2023-09-03 17:29:12 -0300

    :sparkles: fix: Mapa de palco

--- src/assets/data/jobs.js
+++ src/assets/data/jobs.js
@@ -1,18 +1,24 @@
 export const jobs = [
 	{
 		id: 1,
+		id_name: 'mapa-de-palco',
+		tech: 'Mapa de Palco',
+		href: 'files/mapa_de_palco.pdf',
+	},
+	{
+		id: 2,
 		id_name: 'portfolio',
 		tech: 'Portfólio',
 		href: 'files/portfolio.pdf',
 	},
 	{
-		id: 2,
+		id: 3,
 		id_name: 'release',
 		tech: 'Release',
 		href: 'files/release.pdf',
 	},
 	{
-		id: 3,
+		id: 4,
 		id_name: 'clipping',
 		tech: 'Clipping',
 		href: 'files/clipping.pdf',
